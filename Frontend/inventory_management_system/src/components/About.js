b24fc21a2e2d16b08e4539e7c0a487498e59233e
Prarthana <prarthanahegde058@gmail.com>
2024-05-27 23:08:08 +0530

    uploaded from local to cloud

---
+++ Frontend/inventory_management_system/src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='container-fluid p-5'>
+        <h1>wieland</h1>
+        <p>As one of the world's leading suppliers of semi-finished products made of copper and copper alloys as well as other materials such as aluminum, titanium and steel, we support your success with reliable, flexible supply and the best service at consistently high quality. Our products, services and technologies provide sustainable and innovative solutions to today's global challenges such as climate change, digitalization and resource conservation.</p>
+    </div>
+  )
+}
